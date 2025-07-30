6e37d8798e8d403aac121d43125f622d4bc48d5f
Rasyid <arraysdotid@gmail.com>
2025-07-31 03:21:48 +0700

    fix: correct syntax for method chaining in node app

--- apps/api/src/runtime/node.ts
+++ apps/api/src/runtime/node.ts
@@ -8,28 +8,28 @@ import {
   TRPCAdapter,
 } from "../configs";
 
-export const app = new Hono({ strict: true });
+export const app = new Hono({ strict: true })
 
-app.get("/", (c) => {
-  return c.json(getDefaultResponse());
-});
+  .get("/", (c) => {
+    return c.json(getDefaultResponse());
+  })
 
-app.get("/procedure", (c) => {
-  const method = c.req.raw?.method ?? c.req.method;
-  const path = c.req.url;
-  return c.json(getNotFoundResponse({ method, path }), 404);
-});
+  .get("/procedure", (c) => {
+    const method = c.req.raw?.method ?? c.req.method;
+    const path = c.req.url;
+    return c.json(getNotFoundResponse({ method, path }), 404);
+  })
 
-app.all("/procedure/*", async (c) => {
-  return await TRPCAdapter(c.req.raw);
-});
+  .all("/procedure/*", async (c) => {
+    return await TRPCAdapter(c.req.raw);
+  })
 
-app.notFound((c) => {
-  const method = c.req.raw?.method ?? c.req.method;
-  const path = c.req.url;
+  .notFound((c) => {
+    const method = c.req.raw?.method ?? c.req.method;
+    const path = c.req.url;
 
-  return c.json({ error: "Endpoint not found", method, path }, 404);
-});
+    return c.json({ error: "Endpoint not found", method, path }, 404);
+  });
 
 serve({
   port: CONSTANT.PORT,
