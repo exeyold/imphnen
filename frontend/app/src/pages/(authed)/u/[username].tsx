168f92c2bd28e1517c3cb5ef7922ed4e685b994c
Rasyid <arraysdotid@gmail.com>
2025-08-15 06:03:01 +0700

    feat: add basic lanyard components

--- frontend/app/src/pages/(authed)/u/[username].tsx
+++ frontend/app/src/pages/(authed)/u/[username].tsx
@@ -2,10 +2,6 @@ export function meta() {
   return [{ title: "User Special Page" }];
 }
 
-export default function Home() {
-  return (
-    <h1 className="text-base flex justify-center items-center h-screen max-w-xl mx-auto">
-      User Special Page
-    </h1>
-  );
+export default function User() {
+  return <></>;
 }
